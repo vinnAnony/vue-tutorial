8faf0f99faf6be8e4b23d60d8bfea9278129ab54
Vmbugua <vmbugua@cytonn.com>
2022-01-19 08:54:08 +0300

    Components reusability and events, props

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,112 +1,46 @@
-<template>
-    <div class="wrapper">
-        <div class="container">
-            <div class="myForm">
-                <div>
-                    <label for="carName">Vehicle Name</label>
-                    <input type="text"
-                           id="carName"
-                           class="form-control"
-                           v-model="carData.name">
-                </div>
-                <div>
-                    <label for="make">Car Make</label>
-                    <input type="text"
-                           id="make"
-                           class="form-control"
-                           v-model="carData.make">
-                </div>
-
-                <div>
-                    <label for="model">Car Model</label>
-                    <input type="text"
-                           id="model"
-                           class="form-control"
-                           v-model="carData.model">
-                </div>
-
-                <div>
-                    <label for="description">Vehicle description</label>
-                    <textarea id="description"
-                              rows="5"
-                              class="form-control"
-                              v-model="message"/>
-                </div>
-
-                <label for="automatic">
-                    <input type="radio"
-                           id="automatic"
-                           value="Automatic"
-                           v-model="transmissionType"> Automatic
-                </label>
-                <label for="manual">
-                    <input type="radio"
-                           id="manual"
-                           value="Manual"
-                           v-model="transmissionType"> Manual
-                </label>
-                <div>
-                    <label for="fuelType">Fuel Type</label>
-                    <select id="fuelType"
-                            class="form-control"
-                            v-model="selectedFuelType">
-                        <option disabled>Select Fuel Type</option>
-                        <option v-for="(fuelType,index) in fuelTypes" :key="index">{{ fuelType }}</option>
-                    </select>
-                </div>
-
-                <div class="btnWrapper">
-                    <button class="submitBtn" :class="{'is-loading':isSubmitted}"
-                            @click.prevent="submitted">Submit
-                    </button>
-                </div>
-            </div>
-
-            <div class="card" v-if="isSubmitted">
-                <div class="card-body">
-                    <h4 class="card-title">Car Details</h4>
-                    <p>Car Name: <b>{{ carData.name }}</b></p>
-                    <p>Car Make: <b>{{ carData.make }}</b></p>
-                    <p>Car Model: <b>{{ carData.model }}</b></p>
-                    <p>Car Transmission: <b>{{ transmissionType }}</b></p>
-                    <p>Fuel Type: <b>{{ selectedFuelType }}</b></p>
-                    <p>Description: <b>{{ message }}</b></p>
-                </div>
-            </div>
+<template >
+    <Header app-name="Hot Notes" v-on:remove-all="deleteAll"/>
+    <div class="container w-full md:px-40 mx-auto py-20">
+        <div class="md:grid lg:grid-cols-3 md:grid-cols-2 mlg:grid-cols-3 md:gap-10 space-y-6 md:space-y-0 px-10 md:px-0 mx-auto">
+            <Card @remove-note="deleteNote(index)" v-for="(note,index) in notes" :key="index"
+                  v-bind:note-title="note.title"
+                  v-bind:note-content="note.content"/>
         </div>
     </div>
-
+    <Footer/>
 
 </template>
 
 <script>
+    import Header from "./Header";
+    import Card from "./Card";
+    import Footer from "./Footer";
+    // import Container from "./Container";
     export default {
         name: "Home",
+        components: {
+            // Container,
+            Footer,
+            Card,
+            Header
+        },
         data() {
             return {
-                carData: {
-                    name: '',
-                    make: '',
-                    model: '',
-                    transmissionType: "",
-                    selectedFuelType: ""
-
-                },
-                message: 'Describe the vehicle...',
-                transmissionType: '',
-                selectedFuelType: '',
-                fuelTypes: ['Electric', 'Diesel', 'Petrol'],
-                isSubmitted: false,
+                notes: [
+                    { title: "Beez", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dui a turpis tempus, sed blandit odio elementum. Nullam euismod sagittis turpis a tristique. Quisque tincidunt lorem diam, eget consequat elit pulvinar a." },
+                    { title: "Beezy", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dui a turpis tempus, sed blandit odio elementum. Nullam euismod sagittis turpis a tristique. Quisque tincidunt lorem diam, eget consequat elit pulvinar a." },
+                    { title: "Beezer", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dui a turpis tempus, sed blandit odio elementum. Nullam euismod sagittis turpis a tristique. Quisque tincidunt lorem diam, eget consequat elit pulvinar a." },
+                    { title: "Beezest", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dui a turpis tempus, sed blandit odio elementum. Nullam euismod sagittis turpis a tristique. Quisque tincidunt lorem diam, eget consequat elit pulvinar a." },
+                ]
             }
         },
         methods: {
-            submitted() {
-                this.isSubmitted = true;
+            deleteAll(){
+                this.notes = [];
+            },
+            deleteNote(index){
+                this.notes.splice(index, 1);
             }
         },
     }
 </script>
-
-<style scoped>
-
-</style>
